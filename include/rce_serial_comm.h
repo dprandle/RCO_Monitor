cc603b91481b898316da710911cea9e9f0dbd70b
dprandle <dprandle@alaska.edu>
2020-09-18 13:45:48 -0800

    Got command interface working ish

--- include/rce_serial_comm.h
+++ include/rce_serial_comm.h
@@ -1,14 +1,40 @@
 #pragma once
 
+#include <map>
+#include <vector>
+
+#include "shared_structs.h"
 #include "subsystem.h"
+
 
 const uint8_t TMP_BUF_SIZE = 96;
 const uint8_t COMMAND_BUFFER_MAX_SIZE = 20;
 
-const char update_firmware_id[] = "FWU";
-
 class Uart;
 class Timer;
+
+
+/// Subclass this and override process in order to register a new Uart command
+struct Command_Handler
+{
+    Command_Handler()
+    {}
+
+    virtual ~Command_Handler()
+    {}
+
+    virtual bool process(uint8_t * buffer, uint32_t size) = 0;
+
+    Uart * rce_uart;
+};
+
+struct Firmware_Update : public Command_Handler
+{
+    bool process(uint8_t * buffer, uint32_t size);
+    Firmware_Header hdr;
+    uint32_t current_ind;
+    std::vector<uint8_t> payload;
+};
 
 class RCE_Serial_Comm : public Subsystem
 {
@@ -23,6 +49,21 @@ class RCE_Serial_Comm : public Subsystem
 
     void update();
 
+    template<class T>
+    T * add_command(const std::string & command)
+    {
+        T * handler = new T;
+        handler->rce_uart = rce_uart_;
+        auto ins_iter = command_handlers_.emplace(command, handler);
+        if (ins_iter.second)
+            return handler;
+        return nullptr;
+    }
+
+    Command_Handler * get_command(const std::string & command);
+
+    bool remove_command(const std::string & command);
+
     std::string typestr();
 
     static std::string TypeString();
@@ -31,6 +72,10 @@ class RCE_Serial_Comm : public Subsystem
     void check_buffer_for_command_();
 
     Uart * rce_uart_;
-    Timer * timer_;
+
+    std::string current_command;
+
     std::string command_buffer;
+
+    std::map<std::string, Command_Handler *> command_handlers_;
 };
--- src/rce_serial_comm.cpp
+++ src/rce_serial_comm.cpp
@@ -4,7 +4,35 @@
 #include "logger.h"
 #include "timer.h"
 
-RCE_Serial_Comm::RCE_Serial_Comm() : rce_uart_(new Uart(Uart::Uart1)), timer_(new Timer())
+bool Firmware_Update::process(uint8_t * buffer, uint32_t size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (current_ind < FIRMWARE_HEADER_SIZE)
+        {
+            hdr.data[current_ind] = buffer[i];
+        }
+        else
+        {
+            int payload_ind = current_ind - FIRMWARE_HEADER_SIZE;
+            payload[payload_ind] = buffer[i];
+        }
+        ++current_ind;
+
+        // This means we need to resize the payload!
+        if (current_ind == FIRMWARE_HEADER_SIZE)
+        {
+            payload.resize(hdr.byte_size);
+            std::string str("Received byte size of " + std::to_string(hdr.byte_size));
+            rce_uart->write(str.c_str());
+        }
+        else if (current_ind == (FIRMWARE_HEADER_SIZE + hdr.byte_size))
+            return true;
+    }
+    return false;
+}
+
+RCE_Serial_Comm::RCE_Serial_Comm() : rce_uart_(new Uart(Uart::Uart1))
 {
     command_buffer.reserve(COMMAND_BUFFER_MAX_SIZE);
 }
@@ -12,19 +40,19 @@ RCE_Serial_Comm::RCE_Serial_Comm() : rce_uart_(new Uart(Uart::Uart1)), timer_(ne
 RCE_Serial_Comm::~RCE_Serial_Comm()
 {
     delete rce_uart_;
-    delete timer_;
 }
 
 void RCE_Serial_Comm::init()
 {
+    // Create the default commands
+    add_command<Firmware_Update>("FWU");
+
     rce_uart_->set_baud(Uart::b9600);
     Uart::DataFormat df;
     df.db = Uart::d8;
     df.p = Uart::None;
     df.sb = Uart::One;
     rce_uart_->set_format(df);
-
-    timer_->start();
 
     rce_uart_->start();
     rce_uart_->write("\n\rStarting RCO_Monitor\n\r>>> ");
@@ -42,44 +70,86 @@ void RCE_Serial_Comm::update()
     static uint8_t tmp_buf[96];
     uint32_t cnt = rce_uart_->read(tmp_buf, TMP_BUF_SIZE);
 
-    for (int i = 0; i < cnt; ++i)
+    if (current_command.empty())
     {
-        if (tmp_buf[i] == 127)
+        for (int i = 0; i < cnt; ++i)
         {
-            rce_uart_->write("\b \b");
-            command_buffer.pop_back();
-            continue;
+            if (tmp_buf[i] == 127)
+            {
+                rce_uart_->write("\b \b");
+                command_buffer.pop_back();
+                continue;
+            }
+
+            if (tmp_buf[i] == '\r')
+            {
+                rce_uart_->write("\n\r");
+                check_buffer_for_command_();
+                rce_uart_->write("\n\r>>> ");
+                command_buffer.resize(0);
+                continue;
+            }
+
+            if (command_buffer.size() == COMMAND_BUFFER_MAX_SIZE)
+            {
+                wlog("Reached max size of command buffer without command (buffer:{}) - resetting", command_buffer);
+                rce_uart_->write("\n\r>>> ");
+                command_buffer.resize(0);
+            }
+
+            // Echo letters back
+            rce_uart_->write(&tmp_buf[i], 1);
+
+            command_buffer.push_back(tmp_buf[i]);
         }
-
-        if (tmp_buf[i] == '\r')
-        {
-            rce_uart_->write("\n\r");
-            check_buffer_for_command_();
-            rce_uart_->write("\n\r>>> ");
-            command_buffer.resize(0);
-            continue;
-        }
-
-
-        if (command_buffer.size() == COMMAND_BUFFER_MAX_SIZE)
-        {
-            wlog("Reached max size of command buffer without command (buffer:{}) - resetting",command_buffer);
-            rce_uart_->write("\n\r>>> ");
-            command_buffer.resize(0);
-        }
-
-        // Echo letters back
-        rce_uart_->write(&tmp_buf[i],1);
-
-        command_buffer.push_back(tmp_buf[i]);
     }
+    else
+    {
+        Command_Handler * handler = get_command(current_command);
+        if (handler)
+        {
+            if (handler->process(tmp_buf, cnt))
+            {
+                std::string msg = "\n\rCommand: " + current_command + " complete\n\r";
+                current_command.clear();
+                rce_uart_->write(msg.c_str());
+            }
+        }
+        else
+        {
+            current_command.clear();
+        }
+    }
+}
+
+Command_Handler * RCE_Serial_Comm::get_command(const std::string & command)
+{
+    auto fiter = command_handlers_.find(command);
+    if (fiter != command_handlers_.end())
+        return fiter->second;
+    return nullptr;
+}
+
+bool RCE_Serial_Comm::remove_command(const std::string & command)
+{
+    auto fiter = command_handlers_.find(command);
+    if (fiter != command_handlers_.end())
+    {
+        delete fiter->second;
+        command_handlers_.erase(fiter);
+        return true;
+    }
+    return false;
 }
 
 void RCE_Serial_Comm::check_buffer_for_command_()
 {
-    if (command_buffer == update_firmware_id)
+    Command_Handler * handler = get_command(command_buffer);
+    if (handler)
     {
-        rce_uart_->write("Found Firmware Command!");
+        current_command = command_buffer;
+        std::string inv("Executing command: " + command_buffer);
+        rce_uart_->write(inv.c_str());
     }
     else
     {
